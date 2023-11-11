1d27da136261893a3630d611886b96cee1df9813
Myles Skinner <myles.skinner@gmail.com>
2023-11-11 15:05:11 -0500

    Add Searchable concern

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,7 +1,11 @@
 class Magazine < ApplicationRecord
+  include Searchable
+
   has_many :issues
 
   validates_presence_of :slug, :name, :alpha_guide
+
+  scope :name_contains, ->(name) { where('name ILIKE ?', "%#{name}%") }
 
   def to_s
     I18n.t("magazine.name.#{slug}")
