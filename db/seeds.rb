afd44a33ed9c2c74d6f4f23c6a22849273206fc4
Myles Skinner <myles.skinner@gmail.com>
2023-11-11 13:05:56 -0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
+# Examples:
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Role.destroy_all
+
+Role.create! [{ name: 'gsa', position: 99 },
+              { name: 'archivist', position: 80 },
+              { name: 'guest', position: 10 }]
+
+puts "Set up #{Role.count} roles."
+
+if Magazine.count.zero?
+  %w[ahoy cbm cbm_micro power_play cbm_world gazette compute diehard geoworld info run torpet tpug transactor].each do |s|
+    Magazine.create!(
+      slug: s,
+      name: I18n.t("magazine.name.#{s}"),
+      alpha_guide: I18n.t("magazine.name.#{s}").downcase.gsub('the ', '')
+    )
+  end
+  puts "Created #{Magazine.count} magazines."
+else
+  puts 'Magazines have already been seeded.'
+end
