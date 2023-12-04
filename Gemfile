335269ffab24d3f15bd610142ce1f2bb6b1b0e19
Myles Skinner <myles.skinner@gmail.com>
2023-12-04 12:25:07 -0500

    Add issues; simple form (#5)

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "bootsnap", require: false
 # CBM Magazine Index additions
 gem 'cancancan'
 gem 'devise'
+gem 'simple_form'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,46 @@
 class Issue < ApplicationRecord
+  include Searchable
+
   belongs_to :magazine
   has_many :articles
 
   validates_presence_of :year
+
+  scope :by_magazine_id, ->(id) { where(magazine_id: id) }
+  scope :by_year, ->(year) { where(year: year) }
+
+  def to_s
+    "#{magazine.to_s} #{month_display} #{year} (#{sequence})"
+  end
+
+  def date_display
+    "#{month_display[0..2]} #{year}"
+  end
+
+  def magazine_id_display
+    magazine.to_s
+  end
+
+  def month_display
+    month.blank? ? '' : Date::MONTHNAMES[month]
+  end
+
+  def self.month_collection
+    [].tap do |collection|
+      Date::MONTHNAMES.each_with_index { |m, i| collection << [m, i] unless m.nil? }
+    end
+  end
+
+  def friendly_name
+    return special if special.present?
+
+    I18n.t(
+      "issue.friendly_name.#{magazine.slug}",
+      volume: volume,
+      number: number,
+      sequence: sequence,
+      month_name: month.present? ? Date::MONTHNAMES[month] : '',
+      year: year
+    ).squeeze(' ')
+  end
 end
