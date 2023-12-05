82f66d883c8fc31e12db5a26623a7f87b4d51f89
Myles Skinner <myles.skinner@gmail.com>
2023-12-05 00:05:08 -0500

    Articles index with search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
+  include Searchable
+
   belongs_to :issue
+
+  scope :by_sequence, ->(sequence) { joins(:issue).where(issue: { sequence: sequence }) }
+  scope :by_magazine_id, ->(id) { joins(:issue).where(issue: { magazine_id: id }) }
+  scope :by_year, ->(year) { joins(:issue).where(issue: { year: year }) }
 end
