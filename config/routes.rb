6f4680af45d153a0a88680a9abdb61bb61bdb2cc
Myles Skinner <myles.skinner@gmail.com>
2023-12-05 22:14:46 -0500

    Public search form and results table

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'public/about'
+  get 'public/search'
   get 'public/status'
   get 'public/welcome'
 
