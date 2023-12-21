a6d13f936771b539c04e87b746e9b0ee9c540a6c
Myles Skinner <mskinner@infotech.com>
2023-12-21 11:48:07 -0500

    Small fixes

--- app/models/search_log.rb
+++ app/models/search_log.rb
@@ -1,7 +1,7 @@
 class SearchLog < ApplicationRecord
   def self.write_log(params, pagy)
     create!(
-      search_terms: params.to_json,
+      search_terms: params,
       result_count: pagy.count
     )
   end
